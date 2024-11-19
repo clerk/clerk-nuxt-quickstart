d0ccce5469b295ce0d56f4696689786fd169e7ff
wobsoriano <sorianorobertc@gmail.com>
2024-11-19 14:39:03 -0800

    add quickstart example

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    <main>
+        <NuxtPage />
+    </main>
+</template>
